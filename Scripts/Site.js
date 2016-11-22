f421cca728537c4e53117f22cb928948f116bd65
DamonJames <damonmorris16@gmail.com>
2016-11-22 11:56:22 +0900

    completed navbar button

--- Scripts/Site.js
+++ Scripts/Site.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-    $('h2').hide();
-
+    var clicked = false;
     $('.myButton').click(function() {
-        $('body').animate({left: '200px'}, {duration: 1500});
-
+        if (clicked == true) {
+            $('body').animate({left: "-25%"}, {duration: 1000}, queue = false);
+            $('ul').animate({left: "75%"}, 1000);
+            clicked = false;
+        } else {
+            $('body').animate({left: "0"}, {duration: 1000}, queue = false);
+            $('ul').animate({left: "100%"}, 1000);
+            clicked = true;
+        }
     });
 });
