6bfbc5adb081c402bfbebaebf1354ab746bf2998
Reese Morris <reese@halsbury.com>
2017-06-08 09:18:47 +0100

    Added demo component

---
+++ app/democomponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SidebarComponent from './sidebarcomponent.jsx';
+
+class DemoComponent extends React.Component {
+  render () {
+    return (<div> <SidebarComponent /> This is a test demo area  </div>);
+  }
+}
+
+export default DemoComponent;
--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,13 @@ import {render} from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import SidebarComponent from './sidebarcomponent.jsx';
 import AppComponent from './appcomponent.jsx';
+import DemoComponent from './democomponent.jsx';
 //var routes = require('./routes');
 
 render(<BrowserRouter>
       <Switch>
         <Route exact path="/" component={AppComponent} />
         <Route exact path="/home" component={AppComponent} />
+        <Route exact path="/demo" component={DemoComponent} />
       </Switch>
     </BrowserRouter>, document.getElementById('app'));
