614878d8f128e9b527d6c25c8284b85b8ca4360b
Reese Morris <reese@halsbury.com>
2017-06-09 08:39:54 +0100

    Added a test link to the sidebar component

--- app/sidebarcomponent.jsx
+++ app/sidebarcomponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Link} from 'react';
 
 class SidebarComponent extends React.Component {
     constructor(props) {
@@ -8,7 +8,9 @@ class SidebarComponent extends React.Component {
     render() {
         return <div style={{ display: 'inline-block', float: 'left', height: '100%', width:'350px', margin: 'auto', backgroundColor:'#80CBC4' }}> 
             <h2>R Morris</h2>
+            <Link to="/demo"/>
             <ul>
+
                 <li>Projects</li>
                 <li>Blog</li>
                 <li>Contact</li>
